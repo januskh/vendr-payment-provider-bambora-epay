709b0a2c41df9cb63a3749666d547ab5bebedc3e
Janus Kamp Hansen <janus@lukic.nu>
2020-09-19 20:55:51 +0200

    NuGet packages - References adjusted.

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Vendr.PaymentProviders.EPayBambora")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("Bambora ePay (Legacy) Payment Provider for Vendr")]
+[assembly: AssemblyDescription("Payment Provider plugin for Vendr (Umbraco 8) - Developed by Janus Kamp Hansen")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Vendr.PaymentProviders.EPayBambora")]
+[assembly: AssemblyCompany("http://www.kamp-hansen.dk")]
+[assembly: AssemblyProduct("EPay Bambora")]
 [assembly: AssemblyCopyright("Copyright ©  2020")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -33,4 +33,4 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.*")]
